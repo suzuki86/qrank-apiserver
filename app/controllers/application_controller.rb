37729da54d979ef8ec38142e279e4e7e9632a2ba
suzuki86 <tsnr0001@gmail.com>
2014-11-24 21:06:20 +0900

    Show 404 error page when not existing url is called

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActionController::RoutingError, :with => :render_404
+
+  def render_404
+    render :template => "errors/error_404",
+      :status => 404,
+      :layout => "application",
+      :content_type => "text/html"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   mount API => "/"
 
+  get "*path", :controller => "application", :action => "render_404"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
