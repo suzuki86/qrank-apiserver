a53a0913449a6866efb2b7e64bea79a363bbfe93
suzuki86 <tsnr0001@gmail.com>
2014-11-24 22:32:30 +0900

    Add request spec for 404 not found

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 require 'json_expressions/rspec'
 
 describe API do
+  describe "GET /api/v1/something" do
+    it "shows 404 not found" do
+      get "/api/v1/something"
+      expect(response.status).to eq(404)
+      expect(response.body).to include "404 not found"
+    end
+  end
+
   describe "GET /api/v1/entries" do
     it "shows expected entries" do
       FactoryGirl.create(:user_tanaka)
